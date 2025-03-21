d3ced973e5853eaf8c9b09f9256d156e3f274f8e
Jneck <parklim2254@gmail.com>
2025-03-21 18:06:32 +0900

    feat: cache 재설정

--- app/lib/letter/letter.action.ts
+++ app/lib/letter/letter.action.ts
@@ -1,30 +1,33 @@
 'use server'
 
 import { createClient } from '@/utils/supabase/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function insertLetter(content: string) {
   const supabase = await createClient('service');
-  const { data, error } = await supabase
+  try {
+    await supabase
     .from('letter')
-    .insert([{ status: 'pending', content: content }])
-    .select();
-  if (error) {
-    console.error(error);
-    return null;
-  }
-  return data[0];
+    .insert([{ status: 'pending', content: content }])} catch(e) {
+      console.error(e);
+    }
+  
+  revalidatePath('/');
+  redirect('/');
 }
 
 export async function replyLetter(id: number,reply:string) {
   const supabase = await createClient('service');
-  const { data, error } = await supabase
+  try {
+    await supabase
     .from('letter')
     .update({ status: 'replied', reply:reply})
     .eq('id', id)
-    .select();
-  if (error) {
-    console.error(error);
-    return null;
+  } catch(e) {
+    console.error(e)
   }
-  return data[0];
+  revalidatePath('/');
+  revalidatePath(`/post/${id}`);
+  redirect(`/post/${id}`);
 }
